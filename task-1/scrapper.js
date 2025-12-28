251fa7a66c1448503f0464ce3a5a49ccca892d85
Nishu-workspace <nishupatel25112003@gmail.com>
2025-12-28 22:56:13 +0530

    Initial Commit, Adds Basic setup

---
+++ task-1/scrapper.js
@@ -0,0 +1,30 @@
+import puppeteer from "puppeteer";
+const browser = await puppeteer.launch({
+  headless: false,
+  defaultViewport: null,
+  args: ["--start-maximized"],
+});
+const page = await browser.newPage();
+const scrap = async () => {
+  await page.goto("https://beyondchats.com/blogs/", {
+    waitUntil: "networkidle2",
+  });
+  try {
+    const selector = ".is-search-input";
+    await page.locator(selector).click();
+    await page.type(selector, "Google Ads");
+    await page.keyboard.press("Enter");
+
+    // await page.waitForSelector(`.is-search-input`, {
+    //   visible: true,
+    // });
+    // await page.click(".is-search-input");
+    // await page.type(".is-search-input", "Google Ads");
+    //   await page.click(`button[type="submit"]`);
+    await page.screenshot({ path: "example.png" });
+  } catch (err) {
+    console.log("something went wrong", err);
+  }
+  //   await browser.close();
+};
+scrap();
