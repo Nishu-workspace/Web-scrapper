3dfb5a8606979e5e14a2ab692a479d4649ad587c
Nishu-workspace <nishupatel25112003@gmail.com>
2025-12-29 12:22:41 +0530

    feat: add fetches last 5 blogs title and url

--- task-1/scrapper.js
+++ task-1/scrapper.js
@@ -10,19 +10,38 @@ const scrap = async () => {
     waitUntil: "networkidle2",
   });
   try {
+    //gooes to last page.
     const selector = ".page-numbers:last-child";
-    await page.locator(selector).click();
 
-    // await page.locator(selector).click();
-    // await page.type(selector, "Google Ads");
-    // await page.keyboard.press("Enter");
+    const currentPageRef = await page.waitForSelector(selector);
+    let currentPage = parseInt(
+      await page.evaluate((el) => el.textContent, currentPageRef)
+    );
+    console.log(currentPage);
 
-    // await page.waitForSelector(`.is-search-input`, {
-    //   visible: true,
-    // });
-    // await page.click(".is-search-input");
-    // await page.type(".is-search-input", "Google Ads");
-    //   await page.click(`button[type="submit"]`);
+    let allBlogs = [];
+
+    while (allBlogs.length < 5 && currentPage > 0) {
+      console.log(`Scrapping data of ${currentPage}...`);
+
+      await page.goto(`https://beyondchats.com/blogs/page/${currentPage}/`, {
+        waitUntil: "networkidle2",
+      });
+
+      const pageBlogs = await page.$$eval("h2.entry-title a", (links) => {
+        console.log(links);
+        return links.map((l) => ({ title: l.innerText, url: l.href }));
+      });
+      console.log(pageBlogs);
+
+      allBlogs.push(...pageBlogs.reverse());
+      //   console.log(pageBlogs);
+      currentPage--;
+    }
+    const lastFive = allBlogs.slice(0, 5);
+
+    console.log("--- Last 5 Blogs Found ---");
+    console.table(lastFive);
     await page.screenshot({ path: "example.png" });
   } catch (err) {
     console.log("something went wrong", err);
